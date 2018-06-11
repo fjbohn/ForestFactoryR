d859bd405b7ca78b96d5c38ff5111ea2c966168b
Friedrich Bohn angermue <angermue>
2018-06-11 08:59:36 +0200

    initiatlize ff-function including Roxygen-documentation.

---
+++ R/ff.r
@@ -0,0 +1,9 @@
+#' generate forest patches with the forest factory
+#' @description generate various forest patches with the forest factory algorihtm
+#'
+#' @param data characterstring - A forest parameterset; default "temperate_general", Furhter available dataset: "amazon_general"
+#' @export
+
+ff<-function(data="temperate_general"){
+  cat("under construction")
+}
