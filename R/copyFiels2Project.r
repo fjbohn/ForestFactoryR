135d53df9b170d6b7240da30a02d97ba77a6f69f
Friedrich Bohn angermue <angermue>
2018-08-22 13:32:06 +0200

    copy and addpated C++ files of the forest factory scn into github folder

---
+++ R/copyFiels2Project.r
@@ -0,0 +1,29 @@
+#' copy and addpated C++ files of the forest factory scn into github
+#' @description copy and addpated C++ files of the forest factory scn into github
+#'
+#' @param svnPath characterstring - svnPath
+#' @param githubPath characterstring - githubPath
+
+
+
+#githubPath<-"C:/Daten/ownCloud/Projects/H2F3-Paper/ForestFactoryR/src/"
+#svnPath<-"C:/Daten/FORMIND-TRUNK/formind-model/src/"
+
+addLine<-function(file,githubPath){
+  line<-c("#include <Rcpp.h>","using namespace Rcpp;")
+  lines<-readLines(file)
+  lines<-c(line,lines)
+  writeLines(lines,paste0(githubPath,file))
+}
+
+copyFiles2Project<-function(svnPath, githubPath){
+  
+  listFiles<-list.files(path=svnPath,pattern="\\.h$")
+  listFiles<-c(listFiles,list.files(path=svnPath,pattern="\\.cpp$"))
+  setwd(svnPath)
+  dataList<-lapply(listFiles, addLine, githubPath = githubPath )
+
+  
+}
+
+
