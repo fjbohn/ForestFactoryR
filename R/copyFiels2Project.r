0f6f71c65a980f27ce4ae2527faa483cec8c63d3
hnolzen <henning.nolzen@ufz.de>
2018-09-26 10:39:53 +0200

    Source code clean-up

--- R/copyFiels2Project.r
+++ R/copyFiels2Project.r
@@ -5,25 +5,19 @@
 #' @param githubPath characterstring - githubPath
 
 
-
 #githubPath<-"C:/Daten/ownCloud/Projects/H2F3-Paper/ForestFactoryR/src/"
 #svnPath<-"C:/Daten/FORMIND-TRUNK/formind-model/src/"
 
-addLine<-function(file,githubPath){
-  line<-c("#include <Rcpp.h>","using namespace Rcpp;")
-  lines<-readLines(file)
-  lines<-c(line,lines)
-  writeLines(lines,paste0(githubPath,file))
+addLine <- function(file, githubPath){
+  line <- c("#include <Rcpp.h>", "using namespace Rcpp;")
+  lines <- readLines(file)
+  lines <- c(line, lines)
+  writeLines(lines, paste0(githubPath, file))
 }
 
-copyFiles2Project<-function(svnPath, githubPath){
-  
-  listFiles<-list.files(path=svnPath,pattern="\\.h$")
-  listFiles<-c(listFiles,list.files(path=svnPath,pattern="\\.cpp$"))
+copyFiles2Project <- function(svnPath, githubPath){
+  listFiles <- list.files(path = svnPath, pattern = "\\.h$")
+  listFiles <- c(listFiles, list.files(path = svnPath, pattern = "\\.cpp$"))
   setwd(svnPath)
   dataList<-lapply(listFiles, addLine, githubPath = githubPath )
-
-  
 }
-
-
